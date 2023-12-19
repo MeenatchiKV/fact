46da55b620124e971a4fd10fb1f664d39748901b
MeenatchiKV <sandhyavaidhyanathan@gmail.com>
2023-12-19 11:56:33 +0530

    Updated readme.md and added fact.py

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# fact
+# Factorial of a Number
+
+## Aim
+To find the factorial of a given number.
+
+## Procedure
+
+## Code
+
+## Result
---
+++ fact.py
@@ -0,0 +1,13 @@
+import os
+
+def factorial(n):
+    '''
+    Write your code here
+    if n==0:
+        return 1
+    else:
+        return n*factorial(n-1)
+    '''
+
+n=int(input())
+print(f"Factorial of {n} is {factorial(n)}")
